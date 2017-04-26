4197446f6f0277640d9ad3beef5655bf56751427
Ajkea <hendrik.vandijck@student.kdg.be>
2017-04-26 18:55:22 +0200

    update stijl tabel databank

--- web-project/app/Http/Controllers/Controller.php
+++ web-project/app/Http/Controllers/Controller.php
@@ -18,29 +18,31 @@ class Controller extends BaseController
 
     	$campussen = DB::table('campussen')->get();
         //return $campussen;
-    	function build_table($array){
+
+    function build_table($array){
     // start table
-    $html = '<table>';
-    // header row
-    $html .= '<tr>';
-    foreach($array[0] as $key=>$value){
-            $html .= '<th>' . htmlspecialchars($key) . '</th>';
-        }
-    $html .= '</tr>';
+	    $html = '<table>';
+	    // header row
+	    $html .= '<tr>';
+	    foreach($array[0] as $key=>$value){
+	            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+	        }
+	        
+	    $html .= '</tr>';
 
-    // data rows
-    foreach( $array as $key=>$value){
-        $html .= '<tr>';
-        foreach($value as $key2=>$value2){
-            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
-        }
-        $html .= '</tr>';
-    }
+	    // data rows
+	    foreach( $array as $key=>$value){
+	        $html .= '<tr>';
+	        foreach($value as $key2=>$value2){
+	            $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+	        }
+	        $html .= '</tr>';
+	    }
 
-    // finish table and return it
+	    // finish table and return it
 
-    $html .= '</table>';
-    return $html;
+	    $html .= '</table>';
+	    return $html;
 }
         echo build_table($campussen);
 
