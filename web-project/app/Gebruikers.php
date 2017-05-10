444531052051619882dbb646f63f4c2872bf8826
lekevinbm <kevin.bavuidi@hotmail.com>
2017-05-10 22:23:31 +0200

    gebruikers model wa aangepast

--- web-project/app/Gebruikers.php
+++ web-project/app/Gebruikers.php
@@ -3,21 +3,33 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
 class Gebruikers extends Authenticatable
 {
+    use CanResetPassword;
     protected $table = 'gebruikers';
 
  /*   //Deze variabele is nodig want laravel denkt dat onze primary key "id" heet.
     protected $primaryKey = 'gebruiker_id';*/
 
-    /**b
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'voornaam', 'achternaam','email', 'password', 'geboortedatum','geslacht','woonplaats','profielfoto', 'studentenclub','op_kot'
+        'voornaam', 
+        'achternaam',
+        'email',
+        'password',
+        'geboortedatum',
+        'geslacht',
+        'woonplaats',
+        'profielfoto',
+        'studentenclub',
+        'op_kot'
     ];
 
     /**
