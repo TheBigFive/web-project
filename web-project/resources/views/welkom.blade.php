982239590619e257308137ea0d5a54e8ac7327cf
Ajkea <hendrik.vandijck@student.kdg.be>
2017-05-22 11:36:04 +0200

    samenvoegen backend en frontend homepage

--- web-project/resources/views/welkom.blade.php
+++ web-project/resources/views/welkom.blade.php
@@ -1,8 +1,34 @@
 @extends('layouts.app')
 @section('content')
-  <div class="container">
-    <h1>Antwerpen, dé stad voor studenten!</h1>
-    <p>Antwerpen is niet alleen dé stad van de mode, kunst en schelde maar ook van de student! Kom in Antwerpen studeren en ontdek de mooie bekende en onbekende parels die Antwerpen te bieden heeft!</p>            
+  		<div class="hero container">
+  			<div class="mySlides fade">
+	  			<div class="afbeeldingHero container">
+					<img src="{{ asset('img/hero3.png') }}" alt="hero afbeelding">
+				</div>
+			</div>
+  			<div class="mySlides fade">
+	  			<div class="afbeeldingHero container">
+					<img src="{{ asset('img/hero4.png') }}" alt="hero afbeelding">
+				</div>
+			</div>
+  			<div class="mySlides fade">
+	  			<div class="afbeeldingHero container">
+					<img src="{{ asset('img/hero5.png') }}" alt="hero afbeelding">
+				</div>
+			</div>
+		</div>
+
+		<div class="buttons">
+				<span class="dot"></span> 
+				<span class="dot"></span> 
+				<span class="dot"></span> 
+		</div>
+
+
+  	<div class="container inhoud">
+	    <h1>Antwerpen, dé stad voor studenten!</h1>
+	    <p>Antwerpen is niet alleen dé stad van de mode, kunst en schelde maar ook van de student! Kom in Antwerpen studeren en ontdek de mooie bekende en onbekende parels die Antwerpen te bieden heeft!</p>
+	    <h1>Antwerpen, dé stad voor studenten!</h1>
+	    <p>Antwerpen is niet alleen dé stad van de mode, kunst en schelde maar ook van de student! Kom in Antwerpen studeren en ontdek de mooie bekende en onbekende parels die Antwerpen te bieden heeft!</p>   
   </div>
 @endsection
-
