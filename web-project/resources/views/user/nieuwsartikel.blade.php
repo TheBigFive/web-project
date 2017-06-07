7763d37aafe229846ad6b1603652714d73153fe3
Ajkea <hendrik.vandijck@student.kdg.be>
2017-06-07 04:11:13 +0200

    opmaak nieuwsartikel

--- web-project/resources/views/user/nieuwsartikel.blade.php
+++ web-project/resources/views/user/nieuwsartikel.blade.php
@@ -2,6 +2,49 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/nieuwsartikel.css') }}" >
 <div class="container">
-	<h1>Testcontent</h1><h1>{!! $geopendeNieuwsitem->titel !!}</h1>
+	<div class="geopendNieuwsitem">
+		@foreach($alleNieuwsitemMedia as $media)
+				@if($media->mediaType == "Afbeelding")
+					@if($media->isHoofdafbeelding == 1)
+						<div class="heroafbeelding col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<img src="{{ asset($media->link)  }}"></img>
+
+							<div class="titel col-xs-12 col-sm-8 col-md-6 col-lg-6">
+							<h1>{!! $geopendeNieuwsitem->titel !!}</h1>
+							</div>
+						</div>
+					@endif
+				@endif
+			@endforeach
+		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+			<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			<h2>{!! $geopendeNieuwsitem->introtekst !!}</h2>
+			<p>{!! $geopendeNieuwsitem->artikel !!}</p>
+		</div>
+	</div>
+		<div class="extraNieuwsitems col-xs-12 col-sm-4 col-md-4 col-lg-4">
+			<h2>Nieuwe artikels</h2>
+			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
+				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			</div>
+			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
+				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			</div>
+			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
+				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			</div>
+			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
+				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			</div>
+			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
+				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
+			</div>
+
+		</div>
 </div>
-@endsectiopn
+@endsection
