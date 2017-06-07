a1e3bf5ff2ca5ccabd9134c5d59d02d04ef96806
Ajkea <hendrik.vandijck@student.kdg.be>
2017-06-07 12:11:27 +0200

    afwerking pagina nieuwsartikels/id

--- web-project/resources/views/user/nieuwsartikel.blade.php
+++ web-project/resources/views/user/nieuwsartikel.blade.php
@@ -3,26 +3,35 @@
 <link rel="stylesheet" href="{{ asset('css/nieuwsartikel.css') }}" >
 <div class="container">
 	<div class="geopendNieuwsitem">
+
+		<div class="tekstvoor col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<h1 class="titel">{!! $geopendeNieuwsitem->titel !!}</h1>
+			<p class="datum">Toegevoegd op: {{ date('d-m-Y', strtotime($geopendeNieuwsitem->toegevoegdop)) }}</p>
+		</div>
 		@foreach($alleNieuwsitemMedia as $media)
 				@if($media->mediaType == "Afbeelding")
 					@if($media->isHoofdafbeelding == 1)
 						<div class="heroafbeelding col-xs-12 col-sm-12 col-md-12 col-lg-12">
 							<img src="{{ asset($media->link)  }}"></img>
-
-							<div class="titel col-xs-12 col-sm-8 col-md-6 col-lg-6">
-							<h1>{!! $geopendeNieuwsitem->titel !!}</h1>
-							</div>
 						</div>
 					@endif
 				@endif
 			@endforeach
-		<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-			<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
-			<h2>{!! $geopendeNieuwsitem->introtekst !!}</h2>
-			<p>{!! $geopendeNieuwsitem->artikel !!}</p>
+		<div class="inhoudNieuwsitem col-xs-12 col-sm-8 col-md-8 col-lg-8">
+			<h2 class="introtekst">{!! $geopendeNieuwsitem->introtekst !!}</h2>
+			<p class="artikeltekst">{!! $geopendeNieuwsitem->artikel !!}</p>
+			<p class="tag">meer over: <a href="{{ url('nieuwsartikels/4') }}"><button class="tagbutton"> {!! $geopendeNieuwsitem->tag_naam !!}</button></a></p>
 		</div>
-	</div>
-		<div class="extraNieuwsitems col-xs-12 col-sm-4 col-md-4 col-lg-4">
+		<div class="afbeeldingenNieuwsitem col-xs-12 col-sm-4 col-md-4 col-lg-4">
+		@foreach($alleNieuwsitemMedia as $media)
+				@if($media->mediaType == "Afbeelding")
+						<div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
+							<img src="{{ asset($media->link)  }}"></img>
+						</div>
+				@endif
+			@endforeach
+
+			<div class="extraNieuwsitems col-xs-12 col-sm-12 col-md-12 col-lg-12">
 			<h2>Nieuwe artikels</h2>
 			<div class="eerste col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
@@ -44,7 +53,9 @@
 				<a><h3>{!! $geopendeNieuwsitem->titel !!}</h3></a>
 				<p>{!! $geopendeNieuwsitem->tag_naam !!}</p>
 			</div>
-
 		</div>
+	</div>
+	</div>
+
 </div>
 @endsection
